60d0bd15c3be99a22b3c173d4a28a3215270fcfa
Sakula <mohittatu2007@gmail.com>
2025-08-05 12:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ client = SecureAPIClient()
 apis = client.get_apis()
 
 # Global variables
-watermark = "UG"  # Default value
+watermark = "@GovtxExam"  # Default value
 count = 0
 userbot = None
 timeout_duration = 300  # 5 minutes
